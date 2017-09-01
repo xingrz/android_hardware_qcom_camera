2cf334e8111c43041999546e0931a37dd11f86af
VijayaKumar T M <vtmuni@codeaurora.org>
2017-09-01 12:56:05 +0530

    QCamera2/HAL3: Add a null pointer check
    Check null pointer is added
    
    CRs-Fixed: 2053027
    Change-Id: Iddb9fa78c3bb9d735f986f80e2d5df76d784e7e3

--- QCamera2/HAL3/QCamera3HWI.cpp
+++ QCamera2/HAL3/QCamera3HWI.cpp
@@ -2431,6 +2431,11 @@ int QCamera3HardwareInterface::processCaptureRequest(
         for (size_t i = 0; i < request->num_output_buffers; i++) {
             const camera3_stream_buffer_t& output = request->output_buffers[i];
             QCamera3Channel *channel = (QCamera3Channel *)output.stream->priv;
+            if (channel == NULL) {
+                ALOGE("%s: invalid channel pointer for stream", __func__);
+                continue;
+            }
+
             /*for livesnapshot stream is_type will be DIS*/
             if (setEis && output.stream->format == HAL_PIXEL_FORMAT_BLOB) {
                 rc = channel->registerBuffer(output.buffer, IS_TYPE_DIS);
